2a04070d6ac98bdc006cba217014b411c581cf3c
tonijhanel <tonijwilliams@gmail.com>
2023-01-29 01:08:33 -0500

    added aem api method

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,33 +1,6 @@
 import 'dotenv/config'
 import AEMHeadless from '@adobe/aem-headless-client-nodejs';
-const {
-    NODE_APP_HOST_URI,
-    NODE_APP_GRAPHQL_ENDPOINT,
-    NODE_APP_USE_PROXY,
-    NODE_APP_AUTH_METHOD,
-    NODE_APP_DEV_TOKEN,
-    NODE_APP_BASIC_AUTH_USER,
-    NODE_APP_BASIC_AUTH_PASS,
-  } = process.env;
-
-  const setAuthorization = () => {
-    if (NODE_APP_AUTH_METHOD === "basic") {
-      return [NODE_APP_BASIC_AUTH_USER, NODE_APP_BASIC_AUTH_PASS];
-    } else if (NODE_APP_AUTH_METHOD === "dev-token") {
-      return NODE_APP_DEV_TOKEN;
-    } else {
-      // no authentication set
-      return;
-    }
-  };
-
-
-const aemHeadlessClient = new AEMHeadless({
-    serviceURL: 'https://publish-p96606-e882934.adobeaemcloud.com',
-    endpoint: '',           // Avoid non-persisted queries
-    auth: setAuthorization() ,  // accessToken only set if the 2nd command line parameter is set
-  
-});
+import {  aemHeadlessClient } from '$lib/server/aemHeadlessApi';
 
   export const load = async() =>{
         const allAdventures = async()=>{
--- src/routes/adventures/[slug]/+page.svelte
+++ src/routes/adventures/[slug]/+page.svelte
@@ -1,11 +1,49 @@
-<h1>Adventure Details</h1>
-
-
 <script>
 	export let data;
 	const { adventure } = data;
+    import wkndlogo from '$lib/images/wknd-logo-dk.svg';
+    import closeBtn from '$lib/images/closebtn.svg';
 </script>
 
-{#each adventure as adventureDetail}
-<h2>{adventureDetail.activity}</h2>
-{/each}
+<div>
+    <img src={wkndlogo} alt="WKND LOGO"  class="logo"/>
+    <hr>
+    {#each adventure as adventureDetail}
+    <div class="adventure-detail">
+            <button class="adventure-detail-close-button">
+                <a href="/"><img src="{closeBtn}"></a>
+            </button>
+        <h1 class="adventure-detail-title">{adventureDetail.title}</h1>
+        <div class="adventure-detail-info">
+            <div class="adventure-detail-info-label">Activity</div>
+            <div class="adventure-detail-info-description">{adventureDetail.activity}</div>
+            <div class="adventure-detail-info-label">Type</div>
+            <div class="adventure-detail-info-description">{adventureDetail.adventureType}</div>
+            <div class="adventure-detail-info-label">Trip Length</div>
+            <div class="adventure-detail-info-description">{adventureDetail.tripLength}</div>
+            <div class="adventure-detail-info-label">Difficulty</div>
+            <div class="adventure-detail-info-description">{adventureDetail.difficulty}</div>
+            <div class="adventure-detail-info-label">Price</div>
+            <div class="adventure-detail-info-description">
+                <span class="adventure-detail-info-price">{adventureDetail.price}</span>
+            </div>
+        </div>
+        <div class="adventure-detail-content">
+            <img src="{adventureDetail.primaryImage._publishUrl}" class="adventure-detail-primaryimage" alt="adventure image"/>
+            <div>
+                {@html adventureDetail.description.html}
+                <!--<p>{adventureDetail.description.value}</p>-->
+                <h2>Itinerary</h2>
+                <hr />
+                <div class="adventure-detail-itineary">
+                    {@html adventureDetail.itinerary.html} 
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+      
+    {/each}
+</div>
